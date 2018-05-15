ef226fcb213cd48280dacbfad3c4305285e4de6d
fanfpy <fanfpy@qq.com>
2018-05-15 21:24:30 +0800

    登陆
    用微信的登陆接口试试

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,65 +1,36 @@
-// pages/add/add.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    userInfo:null,
+    isUser:false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          that.setData({
+            isUser:true,
+          })
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo),
+              that.setData({
+                userInfo:res.userInfo,
+              })
+            }
+          })
+        }else{
+          that.setData({
+            isUser: false,
+          })
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  bindGetUserInfo: function (e) {
+    console.log(e.detail.userInfo)
   }
 })
